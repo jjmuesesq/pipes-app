02927f239554e9cec8a82aa259dc1695f9532da9
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-26 18:36:34 -0500

    :sparkles: Modulos y rutas de mi aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //carga peresoza
+  {
+    path: '',
+    loadChildren: () => import('./products/products.module').then ( m => m.ProductsModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
