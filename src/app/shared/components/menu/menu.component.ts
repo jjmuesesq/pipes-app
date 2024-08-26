5e799936476d22cf287dda4193e8f0c2668d86d9
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-26 18:00:09 -0500

    :sparkles: PrimeNG - MenuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -10,9 +10,34 @@ export class MenuComponent {
   public menuItems: MenuItem[] = [];
   ngOnInit() {
     this.menuItems = [
-      { label: 'New', icon:'pi pi-fw pi-plus'},
-      { label: 'Open', icon:'pi pi-fw pi-download'},
-      { label: 'Undo', icon:'pi pi-fw pi-refresh'},
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fecha',
+            icon: 'pi pi-align-left'
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe'
+          }
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
+      }
     ];
   }
 }
