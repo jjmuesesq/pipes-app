07d60d53f950deffec4e32f7b568876bf854f356
jjmuesesq <jjmuesesq@unal.edu.co>
2024-09-04 15:29:51 -0500

    :sparkles: sortBy - Pipe personalizado

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ ng g m primeNg
 ng g m products --routing 
 ng g c products/pages/basicsPage --skip-tests
 ng g c shared/components/menu --inline-style --skip-tests
+ng g pipe products/pipes/sortBy --flat --skip-tests
 
 npm install primeng --save
 npm install primeicons --save
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -9,6 +9,7 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { CanFlyPipe } from './pipes/can-fly.pipe';
     UncommonPageComponent,
     OrderComponent,
     ToggleCasePipe,
-    CanFlyPipe
+    CanFlyPipe,
+    SortByPipe
   ],
   imports: [
     CommonModule,
