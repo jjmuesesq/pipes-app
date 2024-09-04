59c97b93cb684c2b888446d1c73a315d36667114
jjmuesesq <jjmuesesq@unal.edu.co>
2024-09-04 00:57:26 -0500

    :sparkles: PrimeNg - Table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,7 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
@@ -17,7 +18,8 @@ import { ToolbarModule } from 'primeng/toolbar';
     CardModule,
     FieldsetModule,
     PanelModule,
-    ToolbarModule
+    ToolbarModule,
+    TableModule
   ]
 })
 export class PrimeNgModule { }
