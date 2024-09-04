f08e84be601ef329842957a896278d6360d64c72
jjmuesesq <jjmuesesq@unal.edu.co>
2024-09-04 17:04:49 -0500

    :sparkles: Ordenar tabla por nombre, vuela y Color

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -8,6 +8,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero;
 
   public heroes: Hero[] = [
     {
@@ -39,5 +40,9 @@ export class OrderComponent {
 
   toggleUpperCase(){
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder( value: keyof Hero ){
+    this.orderBy = value;
   }
 }
